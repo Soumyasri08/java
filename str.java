b55819df60ec9dc9efc41ed093dec57f1be881b6
Soumyasri08 <70061578+Soumyasri08@users.noreply.github.com>
2020-08-27 21:34:56 +0530

    Add files via upload

---
+++ str.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class str{
+public static void main(String args[]){
+
+String name = "sri";
+System.out.println(name);
+Scanner sc = new Scanner(System.in);
+System.out.println("enter a name");
+String name2 = sc.nextLine();
+System.out.println("name2 is:" +name2);
+System.out.println(name + name2);
+String s = "sachin" + "tendulkar";
+System.out.println(s);
+
+}
+}
