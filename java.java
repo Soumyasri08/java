09358b662fa944ccfa93b81adb2eaf91d3752156
Soumyasri08 <70061578+Soumyasri08@users.noreply.github.com>
2020-08-29 18:05:24 +0530

    Add files via upload

---
+++ java.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ class java
+{
+public static void main(String args[])throws FileNotFoundException
+{
+
+String filename = "C:/user/soumy/Desktop/example.txt";
+
+File textfile = new File(filename);
+Scanner sc = new Scanner(textfile);
+
+}
+}
