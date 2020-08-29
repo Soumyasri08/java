c66efb49b28e49a48dd8d5060ba6c104b0517fd5
Soumyasri08 <70061578+Soumyasri08@users.noreply.github.com>
2020-08-29 16:11:42 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,21 @@
+class array
+{
+public static void main(String args[])
+{
+int[] a= new int[5];
+
+a[0]= 20;
+a[1]= 30;
+a[2]= 40;
+
+System.out.println(a[2]);
+System.out.println(a[3]);
+
+String[] fruits={"pear","jack","cherry"};
+
+for(String fruit:fruits)
+{
+System.out.println(fruit);
+}
+}
+}
