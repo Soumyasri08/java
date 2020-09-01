e52f0f0f7773e4a0dce34d7e5a9cc65571506fce
Soumyasri08 <70061578+Soumyasri08@users.noreply.github.com>
2020-09-01 18:54:38 +0530

    Update loop3.java

--- loop3.java
+++ loop3.java
@@ -8,6 +8,7 @@ Scanner sc = new Scanner(System.in);
 
 System.out.println("choose a number from 1 - 3:");
 int n= sc.nextInt();
+//we use the swicth to select one out of many cases for a condition.
 switch(n)
 {
 case 1:
